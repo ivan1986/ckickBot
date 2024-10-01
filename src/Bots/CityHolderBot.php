0aeed56b138f5882b5c6e97d8f3ad1d362512f74
Ivan Borzenkov <ivan.borzenkov@gmail.com>
2024-10-01 16:16:24 +0300

    update all

--- src/Bots/CityHolderBot.php
+++ src/Bots/CityHolderBot.php
@@ -17,6 +17,7 @@ class CityHolderBot extends BaseBot implements BotInterface
 //        $schedule->add(RecurringMessage::every('1 hour', new CustomFunction($this->getName(), 'passiveIncome')));
 //        $schedule->add(RecurringMessage::every('6 hour', new CustomFunction($this->getName(), 'dailyIncome')));
         $schedule->add(RecurringMessage::every('2 hour', new CustomFunction($this->getName(), 'update')));
+        $schedule->add(RecurringMessage::every('24 hour', new CustomFunction($this->getName(), 'updateAll')));
     }
 
     public function saveUrl($client, $url)
@@ -162,6 +163,78 @@ class CityHolderBot extends BaseBot implements BotInterface
         JS);
 
         sleep(5);
+    }
+
+    public function updateAll()
+    {
+        if (!$this->getUrl()) {
+            return;
+        }
+
+        $client = $this->profileService->getOrCreateBrowser($this->curProfile);
+        $client->request('GET', $this->getUrl());
+        sleep(1);
+        $client->waitForElementToContain('body', 'Отлично!');
+        echo 'LOAD' . PHP_EOL;
+        sleep(1);
+        $client->executeScript(<<<JS
+        var my_awesome_script = document.createElement('script');
+        my_awesome_script.setAttribute('src','https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js');
+        document.head.appendChild(my_awesome_script);
+        JS
+        );
+        sleep(2);
+        $client->executeScript(<<<JS
+            $('[class^="_dialogHolderComeBack"]').find('button').click()
+        JS);
+        sleep(2);
+
+        $client->executeScript(<<<JS
+            $('a[href="/city"]')[0].click();
+        JS);
+        sleep(2);
+        $client->executeScript(<<<JS
+            $('a[href="/city/build"]')[0].click();
+        JS);
+        sleep(2);
+
+        // психануть и купить все
+        $client->getWebDriver()->manage()->timeouts()->setScriptTimeout(300);
+        $client->executeScript(<<<JS
+            const sleep = ms => new Promise(r => setTimeout(r, ms));
+            var f3 = async function (tabs) {
+                let shuffled = tabs
+                    .map((i, value) => ({ value, sort: Math.random() }))
+                    .sort((a, b) => a.sort - b.sort)
+                    .map((i, { value }) => value)
+                for(let t of shuffled) {
+                    $(t).click();
+                    await sleep(1000);
+                    let items = $('[class^="_buildPreview"]')
+                        .filter((i, item) => $(item).prop('class').search('disabled') < 0)
+                        .filter((i, item) => $(item).find('[class^="_cooldown"]').length == 0)
+                        .filter((i, item) => $(item).find('button[disabled]').length == 0)
+                    if (items.length == 0) {
+                        continue;
+                    }
+                    await sleep(1000);
+                    $(items[0]).find('button').click();
+                    await sleep(1000);
+                    $('[class^="_buildDetail"] button').click();
+                    await sleep(1000);
+                }
+            };
+            var tabs = $('[class^="_buildNav"] [class^="_navItem"]').filter((i, item) => $(item).find('[class^="_count"]').length>0);
+            while (tabs.length > 0) {
+                await sleep(1000);
+                console.log(tabs);
+                await sleep(1000);
+                f3(tabs);
+                await sleep(1000);
+                tabs = $('[class^="_buildNav"] [class^="_navItem"]').filter((i, item) => $(item).find('[class^="_count"]').length>0);
+                await sleep(1000);
+            }
+        JS);
     }
 
     protected function updateStat($balance)

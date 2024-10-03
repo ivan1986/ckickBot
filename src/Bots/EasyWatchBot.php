fce4d9d4c69fa0ccb39d6e4ae964e4231af7b2e1
Ivan Borzenkov <ivan.borzenkov@gmail.com>
2024-10-03 17:38:23 +0300

    EasyWatch

---
+++ src/Bots/EasyWatchBot.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Bots;
+
+use App\Message\CustomFunction;
+use App\Message\UpdateUrl;
+use Carbon\Carbon;
+use Symfony\Component\Panther\Client;
+use Symfony\Component\Scheduler\RecurringMessage;
+use Symfony\Component\Scheduler\Schedule;
+
+class EasyWatchBot extends BaseBot implements BotInterface
+{
+    public function addSchedule(Schedule $schedule)
+    {
+        $schedule->add(RecurringMessage::every('12 hour', new UpdateUrl($this->getName(), '/k/#@ESWatch_bot'))->withJitter(7200));
+        $schedule->add(RecurringMessage::every('30 min', new CustomFunction($this->getName(), 'checkStream')));
+    }
+
+    public function runInTg(Client $client)
+    {
+        $client->executeScript(<<<JS
+            if (document.querySelector('.reply-markup-button') === null) {
+                document.querySelector('.autocomplete-peer-helper-list-element').click();
+            }
+            document.querySelector('.reply-markup-button').click();
+        JS
+        );
+        sleep(5);
+        parent::runInTg($client);
+    }
+
+    public function checkStream()
+    {
+        if (!$this->getUrl()) {
+            return;
+        }
+        if ($this->cache->get($this->botKey('stream'))) {
+            return;
+        }
+        // Ночью все скипаем
+        if (Carbon::now()->format('H') < 6) {
+            return;
+        }
+
+        $client = $this->profileService->getOrCreateBrowser($this->curProfile, false);
+        $client->get($this->getUrl());
+        $client->request('GET', $this->getUrl());
+        sleep(1);
+        $cookies = $client->getCookieJar();
+        $client->waitForVisibility('[data-test-id="user-balance"]');
+        $balance = $client->executeScript(<<<JS
+            return document.querySelector('[data-test-id="user-balance"]').dataset;
+        JS);
+        $balance = (float)str_replace(' ', '', $balance['title']);
+        $this->updateStat($balance);
+        $client->executeScript(<<<JS
+            document.querySelector('a[href="/stream"]').click();
+        JS);
+        sleep(2);
+        $stream = $client->executeScript(<<<JS
+            return document.querySelector('iframe#stream-live-event') != null;
+        JS);
+
+        $cookiesArray = [];
+        foreach ($cookies->all() as $cookie) {
+            $cookiesArray[] = $cookie->getName() . '=' . $cookie->getValue();
+        }
+        $this->UCSet('cookies', 'cookie: ' . join('; ', $cookiesArray));
+
+        $this->cache->set($this->botKey('stream'), $stream);
+    }
+
+    protected function updateStat($balance)
+    {
+        $balance = round($balance, 3);
+        $gauge = $this->collectionRegistry->getOrRegisterGauge(
+            $this->getName(),
+            'balance',
+            'Balance',
+            ['user']
+        );
+        $gauge->set($balance, [$this->curProfile]);
+        $this->cache->hSet($this->userKey('status'), 'balance', $balance);
+    }
+
+}
---
+++ src/Command/CustomEasyWatchCommand.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Command;
+
+use App\Bots\EasyWatchBot;
+use App\Service\BotSelector;
+use App\Service\CacheService;
+use App\Service\ProfileService;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Process\Process;
+use Symfony\Contracts\Service\Attribute\Required;
+
+#[AsCommand(
+    name: 'custom:easy-watch',
+    description: 'Custom for EasyWatch Bot',
+)]
+class CustomEasyWatchCommand extends Command
+{
+    #[Required] public BotSelector $botSelector;
+    #[Required] public ProfileService $profileService;
+    #[Required] public CacheService $cacheService;
+    public EasyWatchBot $bot;
+
+    protected function configure(): void
+    {
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $this->bot = $this->botSelector->getBot('EasyWatchBot');
+        while (true) {
+            if ($this->cacheService->get($this->bot->botKey('stream'))) {
+                $this->activeStream();
+            } else {
+                sleep(60 * 10);
+            }
+        }
+        return Command::SUCCESS;
+    }
+
+    public function activeStream()
+    {
+        $activeProfiles = $this->getActiveProfiles();
+        /** @var Process[] $processList */
+        $processList = [];
+        while ($this->cacheService->get($this->bot->botKey('stream'))) {
+            foreach ($activeProfiles as $profile => $cookie) {
+                if (empty($processList[$profile]) || !$processList[$profile]->isRunning()) {
+                    if (!empty($processList[$profile])) {
+                        $output = $processList[$profile]->getOutput();
+                        $stop = 'Stream has not been started';
+                        if (str_contains($output, $stop)) {
+                            $this->cacheService->del($this->bot->botKey('stream'));
+                        }
+                    }
+                    $processList[$profile] = $this->getProcess($cookie);
+                    $processList[$profile]->start();
+                }
+            }
+            sleep(5);
+        }
+        $processList = [];
+    }
+
+    /**
+     * @return array
+     */
+    public function getActiveProfiles(): array
+    {
+        $activeProfiles = [];
+        foreach ($this->profileService->list() as $profile) {
+            if ($this->botSelector->isEnabled($profile, 'EasyWatchBot')) {
+                $this->bot->setProfile($profile);
+                $activeProfiles[$profile] = $this->bot->UCGet('cookies');
+            }
+        }
+        return $activeProfiles;
+    }
+
+    protected function getProcess($cookiesStr)
+    {
+        return new Process([
+            'curl',
+            'https://easywatch.tech/wallet/v1/stream/income/events',
+            '-H', $cookiesStr,
+            '-H', 'user-agent: ' . ProfileService::UA,
+            '-H', 'referer: https://easywatch.tech/stream',
+            '-H', 'accept: text/event-stream',
+        ], timeout: 500);
+    }
+}

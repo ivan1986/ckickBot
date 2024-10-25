c66080b5822528d04823e5ac150f744d279861f5
Ivan Borzenkov <ivan.borzenkov@gmail.com>
2024-10-25 11:50:52 +0300

    script for clear chrome caches

---
+++ rm-cache.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+rm -rf profiles/*/optimization_guide_model_store
+rm -rf profiles/*/Default/Cache
+rm -rf profiles/*/Default/Code\ Cache
+rm -rf profiles/*/Default/Service\ Worker/CacheStorage

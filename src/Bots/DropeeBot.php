34f795305c7168b21dd780221236832915ded921
Ivan Borzenkov <ivan.borzenkov@gmail.com>
2024-10-31 12:18:08 +0300

    DropeeBot

---
+++ src/Bots/DropeeBot.php
@@ -0,0 +1,186 @@
+<?php
+
+namespace App\Bots;
+
+use App\Attributes\ScheduleCallback;
+use App\Service\ProfileService;
+use Carbon\Carbon;
+
+class DropeeBot extends BaseBot implements BotInterface
+{
+    public function getTgBotName() { return 'DropeeBot'; }
+
+    public function saveUrl($client, $url)
+    {
+        $url = $this->platformFix($url);
+        parent::saveUrl($client, $url);
+    }
+
+    #[ScheduleCallback('2 hour', delta: 3600)]
+    public function online()
+    {
+        if (!$this->getUrl()) {
+            return;
+        }
+
+        $client = $this->profileService->getOrCreateBrowser($this->curProfile);
+        $client->request('GET', $this->getUrl());
+        sleep(1);
+        $client->waitForVisibility('#base-modal');
+        $auth = $client->executeScript(<<<JS
+            return window.localStorage.getItem('dropee.v1.auth')
+        JS);
+        $auth = json_decode($auth, true);
+        if (empty($auth['token'])) {
+            $this->logger->error($this->getName() . ' for ' . $this->curProfile . ' get token error');
+            $this->UCSet('token', '');
+            return;
+        }
+        $this->cache->hSet(
+            $this->userKey('run'),
+            'updateToken',
+            Carbon::now()->getTimestamp()
+        );
+        $this->UCSet('token', $auth['token']);
+    }
+
+    #[ScheduleCallback('2 hour', delta: 3600)]
+    public function upgrade()
+    {
+        if (!$apiClient = $this->getClient()) {
+            return;
+        }
+
+        $config = $apiClient->get('game/config');
+        $config = $config->getBody()->getContents();
+        $config = json_decode($config, true);
+        $config = $config['config'];
+
+        $sync = $apiClient->post('game/sync');
+        $sync = $sync->getBody()->getContents();
+        $sync = json_decode($sync, true);
+        $sync = $sync['playerStats'];
+
+        $this->updateStatItem('coins', $sync['coins']);
+        $this->updateStatItem('profit', $sync['profit']);
+
+        $myUpgrades = [];
+        foreach ($sync['upgrades'] as $k => $v) {
+            $myUpgrades[$k] = $v['level'];
+        }
+
+        $upgrades = $config['upgrades'];
+        $upgrades = array_filter($upgrades, function ($i) use ($sync, $myUpgrades) {
+            if ($i['price'] >= $sync['coins']) {
+                return false;
+            }
+            if (!empty($i['requirements']['upgrade'])) {
+                $u = $i['requirements']['upgrade'];
+                if (!isset($myUpgrades[$u['id']])) {
+                    return false;
+                }
+                if ($myUpgrades[$u['id']] < $u['level']) {
+                    return false;
+                }
+            }
+            if (!empty($i['requirements']['referrals'])) {
+                $u = $i['requirements']['referrals'];
+                if ($i['requirements']['referrals']['count'] > $sync['referrals']['count']) {
+                    return false;
+                }
+            }
+            if ($i['cooldownUntil'] > time() + 10) {
+                return false;
+            }
+            return true;
+        });
+        usort($upgrades, fn ($a, $b) => $b['profitDelta'] / $b['price'] <=> $a['profitDelta'] / $a['price']);
+
+        if (empty($upgrades)) {
+            return;
+        }
+        $upgrade = current($upgrades);
+        $apiClient->post('game/actions/upgrade', [
+            'json' => ['upgradeId' => $upgrade['id']]
+        ]);
+        $this->cache->hSet(
+            $this->userKey('run'),
+            'realUpgrade',
+            Carbon::now()->getTimestamp()
+        );
+    }
+
+    #[ScheduleCallback('1 hour', delta: 3600)]
+    public function spin()
+    {
+        if (!$apiClient = $this->getClient()) {
+            return;
+        }
+
+        $whell = $apiClient->get('game/fortune-wheel');
+        $whell = $whell->getBody()->getContents();
+        $whell = json_decode($whell, true);
+        $whell = $whell['state'];
+
+        $this->updateStatItem('usdt', $whell['usdtCentsBalance'] / 100);
+
+        if ($whell['spins']['available'] > 0) {
+            $result = $apiClient->post('game/actions/fortune-wheel/spin', [
+                'json' => ['version' => 2]
+            ]);
+            $result = $result->getBody()->getContents();
+            $result = json_decode($result, true);
+            $bonus = $result['prize']['id'];
+            $this->logger->info($this->getName() . ' for ' . $this->curProfile . ' spin bonus: {bonus}', ['bonus' => $bonus]);
+
+            $this->cache->hSet(
+                $this->userKey('run'),
+                'realSpin',
+                Carbon::now()->getTimestamp()
+            );
+        }
+    }
+
+    #[ScheduleCallback('6 hour', delta: 3600)]
+    public function daily()
+    {
+        if (!$apiClient = $this->getClient()) {
+            return;
+        }
+
+        $sync = $apiClient->post('game/sync');
+        $sync = $sync->getBody()->getContents();
+        $sync = json_decode($sync, true);
+        $sync = $sync['playerStats'];
+
+        $rest = 10 - $sync['activities']['watchAdForSpin'];
+        while ($rest > 0) {
+            $s = random_int(40, 130);
+            sleep($s);
+            $apiClient->post('game/actions/extra-spin-by-ad');
+            $rest--;
+        }
+
+        if ($sync['tasks']['dailyCheckin']['lastCheckin'] != date('Y-m-d')) {
+            $apiClient->post('game/actions/tasks/daily-checkin', ['json' => ['timezoneOffset' => -180]]);
+        }
+    }
+
+    protected function getClient(): ?\GuzzleHttp\Client
+    {
+        $token = $this->UCGet('token');
+
+        if (!$token) {
+            return null;
+        }
+
+        return new \GuzzleHttp\Client([
+            'base_uri' => 'https://dropee.clicker-game-api.tropee.com/api/',
+            'headers' => [
+                'Authorization' => 'Bearer ' . $token,
+                'Content-Type' => 'application/json',
+                'User-Agent' => ProfileService::UA,
+            ]
+        ]);
+    }
+}

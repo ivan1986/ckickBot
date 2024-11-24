a5934d2a7c8db3d5c36878969be172836ae51793
Ivan Borzenkov <ivan.borzenkov@gmail.com>
2024-11-24 21:14:09 +0300

    HeartBot

---
+++ src/Bots/HeartBot.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace App\Bots;
+
+use App\Attributes\ScheduleCallback;
+use App\Service\ProfileService;
+
+class HeartBot extends BaseBot implements BotInterface
+{
+    const API = 'https://shark-app-t4cok.ondigitalocean.app/miniapp/';
+
+    public function getTgBotName() { return 'heart_game_bot'; }
+
+    public function saveUrl($client, $url)
+    {
+        $url = $this->platformFix($url);
+
+        $urlFragment = parse_url($url, PHP_URL_FRAGMENT);
+        parse_str($urlFragment, $urlData);
+        $tgData = $urlData['tgWebAppData'];
+        $this->UCSet('tgData', $tgData);
+
+        parse_str($tgData, $tgDataArray);
+        $this->UCSet('hash', $tgDataArray['hash']);
+
+        parent::saveUrl($client, $url);
+    }
+
+    #[ScheduleCallback('10 hour', delta: 7200)]
+    public function tasks()
+    {
+        $apiClient = $this->getClient();
+        $resp = $apiClient->post('auth', [
+            'json' => [
+                'data' => $this->UCGet('tgData')
+            ]
+        ]);
+        $auth = json_decode($resp->getBody()->getContents(), true);
+        $this->updateStatItem('balance', $auth['balance']);
+
+        foreach ($auth['promotions'] as $promotion) {
+            if ($promotion['done']) {
+                continue;
+            }
+            if (in_array($promotion['category'], ['nickname', 'stars_purchase', 'ton_transaction', 'story', 'invite_friends', 'ad'])) {
+                continue;
+            }
+
+            switch ($promotion['category']) {
+                case 'channel':
+                    // todo: subscribe channel
+                    break;
+                case 'telegram':
+                case 'website':
+                    $apiClient->get('https://app.heartgame.fun/api/promotions/' . $auth['id'] . '/' . $promotion['id'], ['allow_redirects' => false]);
+                    sleep(4);
+                    $apiClient->get('promotions/' . $promotion['id'], [
+                        'headers' => [
+                            'Auth-Token' => $this->UCGet('hash')
+                        ]
+                    ]);
+                    return true;
+                default:
+                    $this->logger->error('Unknown promotion category: ' . $promotion['category']);
+                    var_dump($promotion);
+            }
+        }
+    }
+
+    #[ScheduleCallback('1 hour', delta: 900)]
+    public function watchAd()
+    {
+        $apiClient = $this->getClient();
+        $resp = $apiClient->post('auth', [
+            'json' => [
+                'data' => $this->UCGet('tgData')
+            ]
+        ]);
+        $auth = json_decode($resp->getBody()->getContents(), true);
+        foreach ($auth['promotions'] as $promotion) {
+            if ($promotion['done'] || $promotion['category'] != 'ad') {
+                continue;
+            }
+
+            if ($promotion['attempts'] == 0) {
+                return;
+            }
+
+            $client = $this->profileService->getOrCreateBrowser($this->curProfile);
+            $client->get($this->getUrl());
+            sleep(10);
+            $client->executeScript(<<<JS
+                let node = Array.prototype.slice.call(document.querySelectorAll('div.font-semibold')).filter(function (el) {
+                    return el.textContent === 'Watch ads from partners'
+                })[0];
+                node.parentNode.parentNode.parentNode.querySelector('button').click();
+            JS);
+            sleep(5);
+            $existPopup = true;
+            while ($existPopup) {
+                sleep(5);
+                $existPopup = $client->executeScript(<<<JS
+                    return document.querySelectorAll('html > div').length > 0;
+                JS);
+            }
+            return true;
+        }
+    }
+
+    protected function getClient(): ?\GuzzleHttp\Client
+    {
+        return new \GuzzleHttp\Client([
+            'base_uri' => self::API,
+            'headers' => [
+                'Content-Type' => 'application/json',
+                'User-Agent' => ProfileService::UA,
+                'x-requested-with' => 'org.telegram.messenger',
+            ]
+        ]);
+    }
+}

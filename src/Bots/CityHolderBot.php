dcccf4c5d28de2f5dda06957f6d30ebf8186f189
Ivan Borzenkov <ivan.borzenkov@gmail.com>
2025-01-04 00:22:58 +0300

    fix

--- src/Bots/CityHolderBot.php
+++ src/Bots/CityHolderBot.php
@@ -50,7 +50,7 @@ class CityHolderBot extends BaseBot implements BotInterface
             return;
         }
 
-        $client = $this->profileService->getOrCreateBrowser($this->curProfile, false);
+        $client = $this->profileService->getOrCreateBrowser($this->curProfile);
         $client->request('GET', $this->getUrl());
         sleep(2);
 
